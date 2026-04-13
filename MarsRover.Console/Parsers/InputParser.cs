48d481fa9f3e8e30b9af2cafac73b7025dc76201
tomlenton <twlenton@gmail.com>
2026-04-13 14:02:21 +0100

    Feat: Added Parsers folder to hold classes

---
+++ MarsRover.Console/Parsers/InputParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarsRover.App.Enums;
+
+namespace MarsRover.App.Parsers
+{
+    public class InputParser
+    {
+        public List<Instruction> InputParse(string instructions)
+        {
+            return new List<Instruction> { Instruction.L };
+        }
+
+    }
+}
