fd88f5829b7d285115308b9e5ea4014b870e05d0
tomlenton <twlenton@gmail.com>
2026-04-13 14:03:08 +0100

    Did starting test and added second failing test following TDD

--- MarsRover.Tests/UnitTest1.cs
+++ MarsRover.Tests/UnitTest1.cs
@@ -1,6 +1,8 @@
 namespace MarsRover.Tests
 {
-    public class Tests
+using MarsRover.App.Enums;
+    using MarsRover.App.Parsers;
+public class Tests
     {
         public class HelloWorldTests
         {
@@ -8,6 +10,27 @@ namespace MarsRover.Tests
             public void Successfully_runs_a_test()
             {
                 Assert.That(true, Is.True);
+            }
+        }
+        public class ParserTests
+        {
+            [Test]
+            public void InstructionParse_SingleInstruction_ReturnsSingleItemList()
+            {
+                var parser = new InputParser();
+
+                var result = parser.InputParse("L");
+
+                Assert.That(result[0], Is.EqualTo(Instruction.L));
+            }
+            [Test]
+            public void InstructionParse_EmptyString_ReturnsEmptyList()
+            {
+                var parser = new InputParser();
+
+                var result = parser.InputParse("");
+
+                Assert.That(result.Count, Is.EqualTo(0));
             }
         }
     }
