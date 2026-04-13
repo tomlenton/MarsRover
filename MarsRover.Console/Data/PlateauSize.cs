1121826317b05817e314612a7e9aa905a90350ad
tomlenton <twlenton@gmail.com>
2026-04-13 11:37:56 +0100

    Added PlateauSize class

---
+++ MarsRover.Console/Data/PlateauSize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.App.Data
+{
+    internal class PlateauSize
+    {
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+
+        public PlateauSize(int maxX, int maxY)
+        {
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+    }
+}
