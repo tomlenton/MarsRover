a81c34e473e99b4fc15d5eb4a041486f6c0f02db
tomlenton <twlenton@gmail.com>
2026-04-14 14:53:48 +0100

    Feat: Added new PlateauParser

---
+++ MarsRover.Console/Parsers/PlateauParser.cs
@@ -0,0 +1,38 @@
+﻿using MarsRover.App.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.App.Parsers
+{
+    public class PlateauParser
+    {
+        public PlateauSize PlateauParse(string plateauSize)
+        {
+            if (String.IsNullOrEmpty(plateauSize))
+            {
+                throw new ArgumentException("Invalid input, please provide a correct plateau size");
+            }
+            var parts = plateauSize.Split(' ');
+
+            if (parts.Length != 2)
+            {
+                throw new ArgumentException("Plateau size must contain exactly two values");
+            }
+
+            if (!int.TryParse(parts[0], out int maxX))
+            {
+                throw new ArgumentException("Invalid X value for plateau");
+            }
+
+            if (!int.TryParse(parts[1], out int maxY))
+            {
+                throw new ArgumentException("Invalid Y value for plateau");
+            }
+
+            return new PlateauSize(maxX, maxY);
+        }
+    }
+}
